d0682c508723218c7d400cae0ece3ed6e6b86d1a
hussamGhanem <47458864+hussamGhanem@users.noreply.github.com>
2024-03-08 15:54:37 +0100

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,1 +1,1 @@
-python Evaluate/eval.py --pred_file test_mistral/test_mistral_clean/zero.txt --gold_file webnlg_data/test_data/test.source 
+python Evaluate_graphs/eval.py --pred_file test_mistral/test_mistral_clean/zero.txt --gold_file webnlg_data/test_data/test.source 
